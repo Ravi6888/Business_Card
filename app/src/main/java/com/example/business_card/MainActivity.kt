2edb40f8ddcbfe3790a474dbc896506ee51319b5
Ravi6888 <sumanthcp04@gmail.com>
2026-02-26 19:23:45 +0530

    final commit

--- app/src/main/java/com/example/business_card/MainActivity.kt
+++ app/src/main/java/com/example/business_card/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,11 +38,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Business_CardTheme {
-                Surface(modifier = Modifier.fillMaxSize().background(Color(0xFFD2E8D4)),
+                Surface(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFFD2E8D4)),
                     ) {
                     Logobar(
-                        name = "Jennifer Doe",
-                        title = "Android Developer",
+                        name = stringResource(R.string.textname),
+                        title = stringResource(R.string.titlename),
+                    )
+                    Details(
+                        phone = stringResource(R.string.phone_no),
+                        social = stringResource(R.string.social_media),
+                        gmail = stringResource(R.string.gmail)
                     )
                 }
             }
@@ -52,14 +60,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Logobar (name: String, title: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.android_logo)
-    Column(modifier = Modifier.fillMaxSize().background(Color.LightGray),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.LightGray),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         ) {
         Image(
             painter = image,
             contentDescription = null,
-            modifier = Modifier.height(180.dp).width(180.dp).background(Color.DarkGray)
+            modifier = Modifier
+                .height(180.dp)
+                .width(180.dp)
+                .background(Color.DarkGray)
         )
         Text(
             text = name,
@@ -79,7 +92,7 @@ fun Details(phone: String, social: String, gmail: String, modifier: Modifier = M
     Column(
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
         ) {
         Row(verticalAlignment = Alignment.CenterVertically,) {
             Icon(
@@ -125,13 +138,13 @@ fun Details(phone: String, social: String, gmail: String, modifier: Modifier = M
 fun LogobarPreview() {
     Business_CardTheme {
         Logobar(
-            name = "Sumanth Gowda",
-            title = "Android Developer engineer",
+            name = stringResource(R.string.textname),
+            title = stringResource(R.string.titlename),
             )
         Details(
-            phone = "+916360139510",
-            social = "@sumanthgowda6888",
-            gmail = "sumanthcp04@gmail.com"
+            phone = stringResource(R.string.phone_no),
+            social = stringResource(R.string.social_media),
+            gmail = stringResource(R.string.gmail)
         )
     }
 }
